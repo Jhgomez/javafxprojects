7d0d08bb615157fac6aeedd859d4607abef18b40
Juan Gomez <juan.e.g.f@outlook.com>
2025-07-21 23:14:49 -0600

    chore: add correct file address and add configuration to change local cached artifact

--- fxlaunchermavendemo/front/src/main/java/module-info.java
+++ fxlaunchermavendemo/front/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module fxlauncher.front {
     requires javafx.controls;
     requires javafx.fxml;
-    requires fxlauncher.lib;
     requires javafx.graphics;
-
 
     opens fxlauncher.front to javafx.fxml, javafx.graphics;
 
