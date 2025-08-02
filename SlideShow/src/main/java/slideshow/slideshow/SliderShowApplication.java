b4579f13ace9b44db824a5d2bf9a2553f46858d6
Juan Gomez <juan.e.g.f@outlook.com>
2025-08-02 12:14:05 -0600

    chore: set draggable on app

--- SlideShow/src/main/java/slideshow/slideshow/SliderShowApplication.java
+++ SlideShow/src/main/java/slideshow/slideshow/SliderShowApplication.java
@@ -5,11 +5,18 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import slideshow.slideshow.slider.SliderShow;
+import slideshow.slideshow.util.DraggableUtil;
+
 import java.util.Objects;
 
 public class SliderShowApplication extends Application {
+    Stage stage;
+
     @Override
     public void start(Stage stage) {
+        this.stage = stage;
+
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("1.jpg")).toExternalForm()));
         SliderShow sliderShow = new  SliderShow();
         Image I1 = new Image(Objects.requireNonNull(getClass().getResource("1.jpg")).toExternalForm());
         Image I2 = new Image(Objects.requireNonNull(getClass().getResource("2.jpg")).toExternalForm());
@@ -23,8 +30,10 @@ public class SliderShowApplication extends Application {
         sliderShow.setPrefHeight(600);
         sliderShow.setPrefWidth(1024);
 
-        stage.setScene(new Scene(sliderShow));
-        stage.show();
+        this.stage.setScene(new Scene(sliderShow));
+
+        DraggableUtil.setDraggable(sliderShow, this.stage);
+        this.stage.show();
     }
 
     public static void main(String[] args) {
