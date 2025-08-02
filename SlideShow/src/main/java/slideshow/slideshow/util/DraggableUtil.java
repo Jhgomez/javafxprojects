9010ec128fd99bcc63a4fc7236007dfaa970b50c
Juan Gomez <juan.e.g.f@outlook.com>
2025-08-02 14:10:14 -0600

    chore: add oppacity

--- SlideShow/src/main/java/slideshow/slideshow/util/DraggableUtil.java
+++ SlideShow/src/main/java/slideshow/slideshow/util/DraggableUtil.java
@@ -9,12 +9,15 @@ public class DraggableUtil {
     private static double x;
     private static double y;
 
-    public static void setDraggable(Node node, Stage scene) {
+    // this function lets us move the stage by tracking a click and drag on the node component
+    // this means the node should be currently being rendered on the current scene that the stage is showing
+    public static void setDraggable(Node node, Stage stage) {
 
         node.setOnMousePressed(event -> {
             if (event.getButton() == MouseButton.PRIMARY) {
                 x = event.getSceneX();
                 y = event.getSceneY();
+                stage.setOpacity(0.5);
             }
         });
 
@@ -23,8 +26,14 @@ public class DraggableUtil {
                 double newX = event.getScreenX() - x;
                 double newY = event.getScreenY() - y;
 
-                scene.setX(newX);
-                scene.setY(newY);
+                stage.setX(newX);
+                stage.setY(newY);
+            }
+        });
+
+        node.setOnMouseReleased(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                stage.setOpacity(1);
             }
         });
     }
