0df1047b5fef687ce59a1e0630510a4a0180b416
Juan Gomez <juan.e.g.f@outlook.com>
2025-08-02 23:26:41 -0600

    chore: start undecorated stage project

---
+++ UndecoratedStage/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    application
+    id("org.javamodularity.moduleplugin") version "1.8.12"
+    id("org.openjfx.javafxplugin") version "0.1.0"
+    id("org.beryx.jlink") version "3.1.1"
+}
+
+group = "UndecoratedStage"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+application {
+    mainModule.set("slideshow.slideshow")
+    mainClass.set("slideshow.slideshow.SliderShowApplication")
+
+    applicationDefaultJvmArgs = listOf(
+        "-XX:NativeMemoryTracking=summary",
+        "-XX:+UseZGC",
+        "-Xmx400m",
+        "-Xms400m",
+        "-Xss90k",
+        "-XX:MaxGCPauseMillis=200"
+    )
+}
+
+javafx {
+    version = "23"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+jlink {
+
+}
