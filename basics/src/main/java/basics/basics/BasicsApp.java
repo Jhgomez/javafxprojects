5157247945756c502ba85b13f4ec1e8a9a0a52dd
Juan Gomez <juan.e.g.f@outlook.com>
2025-08-08 23:37:13 -0600

    chore: add ellipse with animated circle

--- basics/src/main/java/basics/basics/BasicsApp.java
+++ basics/src/main/java/basics/basics/BasicsApp.java
@@ -1,5 +1,6 @@
 package basics.basics;
 
+import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
@@ -12,9 +13,11 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -25,8 +28,10 @@ public class BasicsApp extends Application {
 //        AnchorPane ap = new AnchorPane();
         ObservableList<Node> nodes = group.getChildren();
 
+        // Drawing a Line
         nodes.add(new Line(10, 100 , 20, 200));
 
+        // Drawing a "rounded "rectangle
         Rectangle rectangle = new Rectangle(50, 20, 250, 180);
         rectangle.setArcHeight(40);
         rectangle.setArcWidth(30);
@@ -38,6 +43,7 @@ public class BasicsApp extends Application {
 
 //        root.getChildren().add(ap);
 
+        // Drawing olympics symbols with using cirles
         Object[][] olympicsSymbolArray = new Object[][] {
                 {100.0, 275.0, 50.0, Color.BLUE},
                 {175.0, 275.0, 50.0, Color.BLACK},
@@ -53,6 +59,26 @@ public class BasicsApp extends Application {
 
             nodes.add(circle);
         }
+
+        // Drawing an planet's orbit using an ellipse and animate it
+        Ellipse orbit = new Ellipse(500, 250, 150, 100);
+        orbit.setFill(Color.WHITE);
+        orbit.setStroke(Color.BLACK);
+
+        // Drawing a circular planet
+        Circle planet = new Circle(500, 150, 40);
+
+        //Creating the animation
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(2000));
+        pathTransition.setNode(planet);
+        pathTransition.setPath(orbit);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(PathTransition.INDEFINITE);
+        pathTransition.setAutoReverse(false);
+        pathTransition.play();
+
+        nodes.addAll(orbit, planet); // is important to add orbit first so planet is draw on top of it
 
         Scene scene = new Scene(group, 520, 440);
         scene.setFill(Paint.valueOf("#fdbf6f"));
