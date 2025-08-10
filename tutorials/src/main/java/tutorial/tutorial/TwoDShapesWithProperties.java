bb1d5a02c76475db2cae3602139713208f964d55
Juan Gomez <juan.e.g.f@outlook.com>
2025-08-09 22:25:43 -0600

    chore: add smooth property

--- tutorials/src/main/java/tutorial/tutorial/TwoDShapesWithProperties.java
+++ tutorials/src/main/java/tutorial/tutorial/TwoDShapesWithProperties.java
@@ -5,7 +5,6 @@ import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.effect.Bloom;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.*;
@@ -27,8 +26,7 @@ public class TwoDShapesWithProperties {
         strokeLineJoinProperty();
         strokeMiterLimitProperty();
         strokeLineCapProperty();
-        drawArc();
-        drawSVGs();
+        smoothProperty();
 
         Scene scene = new Scene(group, 1500, 740);
 
@@ -50,118 +48,29 @@ public class TwoDShapesWithProperties {
     }
 
     /**
-     * SVG (Scalable Vector Graphics) is an XML based language to define vector based graphics. The <path> element in
-     * the SVG library is the most powerful while drawing basic shapes. Using paths, you can draw lines, curves, arcs,
-     * and also various complex shapes including them.
-     *
-     * Even though a path is similar to the polyline element while creating complex shapes, the scale of complex shapes
-     * drawn using a polyline element is not larger than shapes drawn using path element.
-     *
-     * A path in SVG is defined by only one parameter. This parameter holds series of commands, like line, curve or arc
-     * commands. And each of these commands are instantiated using a single letter; for example, the letter 'M' calls the
-     * "Move To" command, the letter 'L' calls the "line" command and 'C' calls "Curve" command. And these letters can
-     * either be specified as either a lowercase or an uppercase letter. The lowercase letter specifies relative
-     * coordinates, while the uppercase letter specifies absolute coordinates.
-     *
-     * The same concept of SVGPath is adopted by JavaFX, in order to create objects.
-     *
-     * In JavaFX we can construct images by parsing SVG paths. Such shapes are represented by the class named SVGPath.
+     * The smooth property in JavaFX is used to smoothen the edges of a certain 2D shape. This property is of the type Boolean. If this value is true, then the edges of the shape will be smooth.
      */
-    private void drawSVGs() {
-        SVGPath triangleSvgPath = new SVGPath();
-        String triagnlePath = "M 100 100 L 300 100 L 200 300 z";
-        //Setting the SVGPath in the form of string
-        triangleSvgPath.setContent(triagnlePath);
-        triangleSvgPath.setLayoutX(885);
-        triangleSvgPath.setLayoutY(420);
+    private void smoothProperty() {
+        Circle circle = new Circle(850.0, 450.0, 200.0);
 
-        SVGPath bezierSvgPath = new SVGPath();
+        circle.setFill(Color.BLUE);
+        circle.setStroke(Color.BLACK);
+        circle.setStrokeWidth(16);
+        circle.setSmooth(true);
 
-        String bezierPath = "M 70 110 C 70 180, 210 180, 210 110";
+        Circle circle1 = new Circle(1275.0, 450.0, 200.0);
 
-        //Setting the SVGPath in the form of string
-        bezierSvgPath.setContent(bezierPath);
+        circle1.setFill(Color.BLUE);
+        circle1.setStroke(Color.BLACK);
+        circle1.setStrokeWidth(16);
+        circle1.setSmooth(false);
 
-        // Setting the stroke and fill of the path
-        bezierSvgPath.setStroke(Color.BLACK);
-        bezierSvgPath.setFill(Color.ORANGE);
-
-        bezierSvgPath.setLayoutX(1150);
-        bezierSvgPath.setLayoutY(420);
-
-        Text pathText = new Text("SVGs Paths, drawing a triangle and bezier curve using SVG Paths");
-        pathText.setFont(new Font(16));
-        pathText.setX(970);
-        pathText.setY(517);
-
-        nodes.addAll(triangleSvgPath, bezierSvgPath, pathText);
-    }
-
-    /**
-     * An arc in simple geometry is defined as a portion of a circumference of an ellipse or a circle.
-     * It will have the following properties
-     *  - startAngle − The starting angle of the arc in degrees.
-     *  - length − The angular extent of the arc in degrees.
-     *  - radiusX − The width of the full Ellipse of which the current arc is a part of.
-     *  - radiusY − The height of the full Ellipse of which the current arc is a part of.
-     *
-     *  If both radiusX and radiusY are same, then the arc is a part of a circle circumference.
-     *
-     *  In JavaFX, you can draw three kinds of arcs namely
-     *  - Open − An arc which is not closed at all is known as an open arc.
-     *  - Chord − A chord is a type of an arc which is closed by straight line.
-     *  - Round − The Round arc is an arc which is closed by joining the starting and ending point to the center of the ellipse.
-     *
-     *  You can set the type of the arc using the method setType() by passing any of the following properties − ArcType.OPEN,
-     *  ArcType.CHORD, ArcType.Round.
-     */
-    private void drawArc() {
-        Arc arc = new Arc();
-
-        arc.setCenterX(750.0);
-        arc.setCenterY(555.0);
-        arc.setRadiusX(50.0f);
-        arc.setRadiusY(50.0f);
-        arc.setStartAngle(40.0f);
-        arc.setLength(239.0f);
-
-        arc.setType(ArcType.ROUND);
-        arc.setStroke(Color.RED);
-
-        Arc arc2 = new Arc();
-
-        arc2.setCenterX(840.0);
-        arc2.setCenterY(555.0);
-        arc2.setRadiusX(50.0f);
-        arc2.setRadiusY(50.0f);
-        arc2.setStartAngle(40.0f);
-        arc2.setLength(239.0f);
-
-        arc2.setFill(Color.TRANSPARENT);
-        arc2.setStroke(Color.BLACK);
-
-        arc2.setType(ArcType.OPEN);
-
-        Arc arc3 = new Arc();
-
-        arc3.setCenterX(930.0);
-        arc3.setCenterY(555.0);
-        arc3.setRadiusX(50.0f);
-        arc3.setRadiusY(50.0f);
-        arc3.setStartAngle(40.0f);
-        arc3.setLength(239.0f);
-
-        arc3.setFill(Color.TRANSPARENT);
-        arc3.setStroke(Color.BLACK);
-
-        arc3.setType(ArcType.CHORD);
-
-        Text circleText = new Text("Arcs types, Round, Open and Chord");
+        Text circleText = new Text("Circles, first with setSmooth \"true\"(default val in all shapes), second set to false");
         circleText.setFont(new Font(16));
-        circleText.setX(710);
-        circleText.setY(497.0);
+        circleText.setX(830);
+        circleText.setY(230);
 
-        nodes.addAll(arc, arc2, arc3, circleText);
+        nodes.addAll(circle, circle1, circleText);
     }
 
     /**
