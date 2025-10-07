a9f1b5d7f5096adfd693ab0d0718caa824432ca5
Juan Gomez <juan.e.g.f@outlook.com>
2025-10-06 20:50:10 -0600

    chore: add record class

---
+++ tutorials/src/main/java/tutorial/tutorial/examples/networking/udp/OutputSetUp.java
@@ -0,0 +1,5 @@
+package tutorial.tutorial.examples.networking.udp;
+
+import java.io.*;
+
+public record OutputSetUp(ByteArrayOutputStream byteArrayOutputStream, ObjectOutputStream objectOutputStream) { }
